6c8160213fe1bc4390001c7d0104c41ded2eb498
Caio Rulli <caiorulli@gmail.com>
2019-05-09 21:41:20 -0300

    I'm really taking my time with this.

--- src/dangan_clj/core.clj
+++ src/dangan_clj/core.clj
@@ -1,21 +1,28 @@
 (ns dangan-clj.core
   (:gen-class))
 
-(defn make-player []
+(defn- make-player []
   {:clues #{}})
 
 (defn make-poi [name clue]
   {:name name :clue clue})
 
-(defn interact [player poi]
-  (assoc player :clues
-         (conj (:clues player) (:clue poi))))
+(defn make-initial-state [scene]
+  {:player (make-player)
+   :scene scene})
 
-(defn interact-with [poi-name player scene]
-  (interact player
-            (first (clojure.set/select
-                    #(= (:name %) poi-name)
-                    (:pois scene)))))
+(defn- interact [player poi]
+  (if (nil? poi)
+    player
+    (assoc player :clues
+           (conj (:clues player) (:clue poi)))))
+
+(defn interact-with [poi-name state]
+  (assoc state :player
+         (interact (:player state)
+                   (first (clojure.set/select
+                           #(= (:name %) poi-name)
+                           (:pois (:scene state)))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/dangan_clj/core_test.clj
+++ test/dangan_clj/core_test.clj
@@ -1,9 +1,8 @@
 (ns dangan-clj.core-test
   (:require [midje.sweet :refer [fact facts =>]]
-            [dangan-clj.core :refer [make-player
-                                     make-poi
+            [dangan-clj.core :refer [make-poi
                                      interact-with
-                                     interact]]))
+                                     make-initial-state]]))
 
 (def clue-1 {:id 1})
 
@@ -12,15 +11,28 @@
 (def test-scene
   {:pois #{knife}})
 
-(facts "about the player interaction"
-       (fact "player should start clueless"
-             (:clues (make-player)) => #{})
+(def initial-state (make-initial-state test-scene))
 
-       (fact "player should not be able to add same clue twice"
-             (let [player (make-player)
-                   player-with-clue (interact player knife)]
-               (:clues (interact player-with-clue knife)) => #{clue-1}))
+(facts
+ "about the player interaction"
+ (fact "player should start clueless"
+       (-> initial-state
+           :player
+           :clues) => #{})
 
-       (fact "player should add clues by interacting with poi"
-             (let [player (make-player)]
-               (:clues (interact-with "knife" player test-scene)) => #{clue-1})))
+ (fact "player should not be able to add same clue twice"
+       (let [already-interacted-state (interact-with "knife" initial-state)]
+         (-> (interact-with "knife" already-interacted-state)
+             :player
+             :clues) => #{clue-1}))
+
+ (fact "player should add clues by interacting with poi"
+       (-> (interact-with "knife" initial-state)
+           :player
+           :clues) => #{clue-1})
+
+ (fact "player interacting with non-existing poi should not return different player"
+       (interact-with "balloon" initial-state) => initial-state))
+
+(facts
+ "about dialog mode")
